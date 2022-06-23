3c646e19a50601b30a849f565ac32ce182e47adf
zainkhanbaloch <mzainabbas77@gmail.com>
2022-06-23 00:56:48 -0700

    Adding files

---
+++ javascript/resp.js
@@ -0,0 +1,21 @@
+let navbar = document.getElementById("navbar")
+let logo = document.getElementById("mainlogo")
+let hamburg = document.getElementById("hamburger")
+
+hamburg.addEventListener("click", function run(){
+    if (navbar.style.display != "none"){
+        navbar.style.display = "none"
+        
+    }
+    else{
+        navbar.style.display = "flex"
+    }
+    // alert("You clicked logo")
+})
+
+// let closer = document.getElementById("close")
+// closer.addEventListener("click", function run(){
+//     if (navbar.style.display != "none"){
+//         navbar.style.display = "none"
+//     }
+// })
