5586c3b48fa545df241cc82ccd76821ecc9b7c7f
“Zain_Baloch” <mzainabbas77@gmail.com>
2022-07-14 16:30:52 +0500

    Updated

--- javascript/resp.js
+++ javascript/resp.js
@@ -7,11 +7,18 @@ hamburg.addEventListener("click", function run(){
         navbar.style.display = "none"
         
     }
-    else if (navbar.style.display = "none"){
+    else {
         navbar.style.display = "flex"
     }
     // alert("You clicked logo")
 })
+
+// let closer = document.getElementById("close")
+// closer.addEventListener("click", function run(){
+//     if (navbar.style.display != "none"){
+//         navbar.style.display = "none"
+//     }
+// })
 
 // let closer = document.getElementById("close")
 // closer.addEventListener("click", function run(){
